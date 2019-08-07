5b3afeeba90991194d6e1a0d9f8d594f0de55758
Syed Moizullah <syed.moizullah@aofl.com>
2019-08-07 10:52:23 -0700

    help me sort these

---
+++ numbers/all_numbers.php
@@ -0,0 +1,1 @@
+<?php/** * Created by PhpStorm. * User: syed.moizullah * Date: 8/7/19 * Time: 10:45 AM */$list_of_numbers = array(1,2,3,4,5,6,6,88,45,3,22,15,9,32);
---
+++ numbers/sort.php
@@ -0,0 +1,1 @@
+<?php/** * Created by PhpStorm. * User: syed.moizullah * Date: 8/7/19 * Time: 10:45 AM */include_once __DIR__ . "/all_numbers.php";for($i = 0; $i < count($list_of_numbers); $i++) {    echo $list_of_numbers[$i];    echo "\n";}
